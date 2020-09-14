7801bcfbf2e7a0c603033919b838b37436f35ad9
xuhuihui <494655043@qq.com>
2020-09-14 21:26:13 +0800

    todo 13: add async test with manual mock

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,10 @@
+const { default: getUsers } = require("../users");
+
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
+  jest.mock("axios");
+
+  test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
+    await expect(getUsers()).resolves.toBe("response data");
   });
 });
