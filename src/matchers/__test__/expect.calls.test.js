2117ee14fd43c5fe02561c4ee398757fcab9b5b3
xuhuihui <494655043@qq.com>
2020-09-14 22:06:40 +0800

    matchers

--- src/matchers/__test__/expect.calls.test.js
+++ src/matchers/__test__/expect.calls.test.js
@@ -7,6 +7,11 @@ test("TODO 11", () => {
   forEach([1, 2], mockCallback);
 
   // TODO 11: add assertion
+  expect(mockCallback).toHaveBeenCalledTimes(2);
+  expect(mockCallback).toHaveBeenCalledWith(1);
+  expect(mockCallback).toHaveBeenCalledWith(2);
+  expect(mockCallback).toReturnWith(43);
+  expect(mockCallback).toReturnWith(44);
 });
 
 test("TODO 12", () => {
@@ -17,8 +22,8 @@ test("TODO 12", () => {
     .mockReturnValue(true);
 
   // TODO 12: to add "expected" value
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
+  expect(mockFn()).toBe(42);
+  expect(mockFn()).toBe("string");
+  expect(mockFn()).toBe(true);
+  expect(mockFn()).toBe(true);
 });
